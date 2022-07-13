1c72ec2ea186e8b216d8126186b8c287bc0c18a1
WendellRodrigues <robertowendellrodrigues@gmail.com>
2022-07-13 15:47:41 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
 
 function App() {
   
   return(
     <>
- <h1>Olá React 2</h1>
-  <NotificationButton/>    
-  <NotificationButton/>  
-  <NotificationButton/>    <NotificationButton/>    <NotificationButton/>    <NotificationButton/>        
+<main>
+      <section id="sales">
+        <div className="dsmeta-container"></div>
+        
+        <Header/>
+        <SalesCard />
+        </section>
+        </main>
+ 
+ 
+ 
   </>
   )
 }
