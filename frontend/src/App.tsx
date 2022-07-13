1c10331eb605c10103e1491bb5568d7351477a8c
WendellRodrigues <robertowendellrodrigues@gmail.com>
2022-07-13 11:39:39 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   
   return(
+    <>
  <h1>Olá React 2</h1>
-        
+  <NotificationButton/>    
+  <NotificationButton/>  
+  <NotificationButton/>    <NotificationButton/>    <NotificationButton/>    <NotificationButton/>        
+  </>
   )
 }
 
